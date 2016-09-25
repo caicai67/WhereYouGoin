04db3675f1e45eebb65f3e98c005da94b1a45cd5
Jennifer Ma <jennifer.ma@gatech.edu>
2016-09-24 23:50:21 -0400

    edit main

--- app/main.php
+++ app/main.php
@@ -13,6 +13,8 @@ $tablename = "flight";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
+echo $conn;
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
