12e4614fd0e4af20358ab042a11917f762873b9f
Jennifer Ma <jennifer.ma@gatech.edu>
2016-09-24 22:21:54 -0400

    update main

--- app/main.php
+++ app/main.php
@@ -1,18 +1,3 @@
-<!-- <?php
-
-require '../vendor/autoload.php';
-$app = new \Slim\Slim();
-
-$app->get('/', function() use($app){
-	$app->response->setStatus(200);
-	echo "Welcome to slim !";
-
-});
-
-$app->run();
-
-?> -->
-
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
