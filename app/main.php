8855777a347b4a46cb2346c822cb29155b2e32e6
Jennifer Ma <jennifer.ma@gatech.edu>
2016-09-24 22:26:56 -0400

    remove comment

--- app/main.php
+++ app/main.php
@@ -1,17 +1,4 @@
-<!-- <?php
 
-require '../vendor/autoload.php';
-$app = new \Slim\Slim();
-
-$app->get('/', function() use($app){
-	$app->response->setStatus(200);
-	echo "Welcome to slim !";
-
-});
-
-$app->run();
-
-?> -->
 
 <?php
 // use \Psr\Http\Message\ServerRequestInterface as Request;
